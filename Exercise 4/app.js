254bf6a9b7d84ca8220fcb21f5ec87dac9d04f99
Huzaifa206 <160318673+Huzaifa206@users.noreply.github.com>
2024-03-18 00:30:36 +0500

    Add files via upload

---
+++ Exercise 4/app.js
@@ -0,0 +1,6 @@
+"use strict";
+//store the quote and persons name in var
+const quote = "To fight against one’s desires is the greatest of all fights.";
+const author = "Salahuddin Ayyubi";
+//Now print the quote with persons name
+console.log(`${author} once said, "${quote}"`);
---
+++ Exercise 4/app.ts
@@ -0,0 +1,7 @@
+//store the quote and persons name in var
+const quote: string = "To fight against one’s desires is the greatest of all fights.";
+const author: string = "Salahuddin Ayyubi";
+
+//Now print the quote with persons name
+console.log(`${author} once said, "${quote}"`);
+
